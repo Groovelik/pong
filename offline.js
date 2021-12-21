0e942762e8d61e3fa2cf7843133b900db9039b0f
Groovelik <96476437+Groovelik@users.noreply.github.com>
2021-12-21 16:45:30 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1640086408,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/barrier-sheet0.png",
+		"images/fon.png",
+		"images/player1-sheet0.png",
+		"images/player2-sheet0.png",
+		"images/p1pont-sheet0.png",
+		"images/p2pont-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/barer-sheet0.png",
+		"images/fonn-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
